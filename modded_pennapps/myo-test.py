99682e5df0664b830f2e474254aa10c5e54725b3
John Lore <johnlore@MacBook-Pro.local>
2016-01-12 23:42:07 -0500

    added pennapps and commented out myo stuff

---
+++ modded_pennapps/myo-test.py
@@ -0,0 +1,11 @@
+import Armband
+import time
+
+a = Armband.Armband()
+while True:
+    pass
+    #print "yaw ", a.collector.yaw_w
+    #print "roll ", a.collector.roll_w
+    #print "pitch ", a.collector.pitch_w
+    #print "all_shots ", a.collector.all_shots
+    #print "one_shot", a.collector.one_shot
